a3189f225e335ac92fcc95914196b52b4226e00f
Edward Fernando <edward.fer@gmail.com>
2013-10-11 23:29:12 +0700

    New AbstractDAO

---
+++ src/main/java/mozart/core/db/model/Model.java
@@ -0,0 +1,29 @@
+package mozart.core.db.model;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+@XmlAccessorType(value = XmlAccessType.FIELD)
+@MappedSuperclass
+public class Model {
+
+	@Id
+	@GeneratedValue
+	@XmlElement
+	private Long id;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+}
