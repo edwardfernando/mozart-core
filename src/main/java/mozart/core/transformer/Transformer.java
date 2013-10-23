566f86f73426288da9fc8df4525ada3f5b034dca
Edward Fernando <edward.fer@gmail.com>
2013-10-23 21:37:00 +0700

    Add new validator method for Transformer

--- src/main/java/mozart/core/transformer/Transformer.java
+++ src/main/java/mozart/core/transformer/Transformer.java
@@ -8,4 +8,12 @@ public abstract class Transformer<T> {
 
 	public abstract T transform(Field field, String value) throws MozartException;
 
+	protected T validate(T t, Field field, String value) throws MozartException {
+		if (t == null) {
+			throw new MozartException(String.format("%s with id %s is not found", field
+			    .getType()
+			    .getSimpleName(), value));
+		}
+		return t;
+	}
 }
