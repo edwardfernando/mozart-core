f26a85e6836552914adf61bad7f22df5e36f8a36
Edward Fernando <edward.fer@gmail.com>
2013-11-04 16:01:31 +0700

    Move execute function to be handled inside this class

--- src/main/java/mozart/core/pagination/FilterableQuery.java
+++ src/main/java/mozart/core/pagination/FilterableQuery.java
@@ -2,8 +2,10 @@ package mozart.core.pagination;
 
 import java.util.List;
 
+import mozart.core.db.AbstractDAO;
 import mozart.core.exception.MozartException;
 
+import org.hibernate.Query;
 import org.hibernate.Session;
 
 public abstract class FilterableQuery {
@@ -36,6 +38,16 @@ public abstract class FilterableQuery {
 		this.filterableDao = filterableDao;
 	}
 
-	public abstract List<?> execute() throws MozartException;
+	public abstract String getQuery();
+
+	public List<?> execute() throws MozartException {
+		Query query = session.createQuery(getQuery());
+
+		query.setFirstResult(filterCriteria.start());
+		query.setMaxResults(filterCriteria.getMax());
+
+		AbstractDAO<?> dao = (AbstractDAO<?>) getFilterableDao();
+		return (List<?>) dao.execList(query);
+	}
 
 }
