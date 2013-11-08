7b75f3dab77774f0d0f9d915cd84747c0881eb20
Edward Fernando <edward.fer@gmail.com>
2013-11-08 22:37:12 +0700

    New advertiserType data source & json file

---
+++ src/main/java/mozart/core/datasource/AdvertiserTypeDataSource.java
@@ -0,0 +1,68 @@
+package mozart.core.datasource;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.util.List;
+import java.util.Map;
+
+import mozart.core.datasource.pojo.AdvertiserType;
+
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import com.google.gson.Gson;
+import com.google.gson.stream.JsonReader;
+
+public class AdvertiserTypeDataSource {
+
+	private static Map<String, AdvertiserType> map = Maps.newConcurrentMap();
+	private static List<AdvertiserType> list = Lists.newArrayList();
+
+	private static AdvertiserTypeDataSource instance;
+
+	private AdvertiserTypeDataSource() {
+		initialize();
+	}
+
+	private void initialize() {
+		InputStream streams = getClass().getClassLoader().getResourceAsStream(
+		    "jsons/advertiser_type.json");
+
+		Gson gson = new Gson();
+		JsonReader reader;
+		try {
+			reader = new JsonReader(new InputStreamReader(streams, "UTF-8"));
+			reader.beginArray();
+			while (reader.hasNext()) {
+				AdvertiserType type = gson.fromJson(reader, AdvertiserType.class);
+				map.put(type.getCode(), type);
+				list.add(type);
+			}
+
+			reader.endArray();
+			reader.close();
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public Map<String, AdvertiserType> getMap() {
+		return map;
+	}
+
+	public List<AdvertiserType> getList() {
+		return list;
+	}
+
+	public static AdvertiserTypeDataSource instance() {
+		if (instance == null) {
+			instance = new AdvertiserTypeDataSource();
+		}
+		return instance;
+	}
+
+}
---
+++ src/main/java/mozart/core/datasource/pojo/AdvertiserType.java
@@ -0,0 +1,34 @@
+package mozart.core.datasource.pojo;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+@XmlAccessorType(value = XmlAccessType.FIELD)
+public class AdvertiserType {
+
+	@XmlElement
+	private String code;
+
+	@XmlElement
+	private String name;
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
